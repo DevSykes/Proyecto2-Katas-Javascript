46fc78fbb87de3b76d8e4b6347433fd5aa607744
S.Z. <234208376+DevSykes@users.noreply.github.com>
2026-03-03 17:51:49 +0100

    docs: agregar enunciados

--- ejercicios-basicos-avanzados/ejercicio-02.js
+++ ejercicios-basicos-avanzados/ejercicio-02.js
@@ -1,8 +1,20 @@
+//Modifica el siguiente código JavaScript para actualizar la edad de Luke Skywalker a 25 años.
+
 const jedi = {nombre: "Luke Skywalker", edad: 19};
 jedi.edad = 25;
 console.log(jedi);
 
 // =============================
+
+/* Crea tres variables con la siguiente información:
+nombre: "Leia"
+apellido: "Organa"
+edad: 20
+
+Muestra un mensaje por consola que siga la siguiente estructura:
+"Soy Leia Organa, tengo 20 años y soy una princesa de Alderaan."
+
+Utiliza la concatenación para ello. */
 
 let nombre = "Leia";
 let apellido = "Organa";
@@ -11,6 +23,11 @@ let edad = 20;
 console.log("Soy " + nombre + " " + apellido + ", tengo " + edad + " años y soy una princesa de Alderaan.");
 
 // =============================
+/*Obtén el precio total de dos sables de luz: "Shoto de Yoda" y "sable de Darth Vader".
+Imprime el resultado por consola utilizando console.log.
+const sable1 = {nombre: "Shoto de Yoda", precio: 1500};
+const sable2 = {nombre: "Sable de Darth Vader", precio: 2000};
+*/
 
 const sable1 = {nombre: "Shoto de Yoda", precio: 1500};
 const sable2 = {nombre: "Sable de Darth Vader", precio: 2000};
@@ -19,6 +36,15 @@ const precioTotal = sable1.precio + sable2.precio;
 console.log("El precio total de los sables es: " + precioTotal);
 
 // =============================
+/*Modifica el valor de la variable global precioBaseGlobal a 25.000 créditos.
+Actualiza el precio final (precioFinal) de dos naves ("Ala-X" y "Halcón Milenario") sumando el valor de precioBaseGlobal a su precio base (precioBase).
+let precioBaseGlobal = 10000;
+
+Cambia el precio base en esta linea modificando la variable
+
+const nave1 = {nombre: "Ala-X", precioBase: 50000, precioFinal: 60000};
+const nave2 = {nombre: "Halcón Milenario", precioBase: 70000, precioFinal: 80000};*/
+
 
 let precioBaseGlobal = 10000;
 
