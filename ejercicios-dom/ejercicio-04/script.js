8a5ed07254e1840d48affa9a50b35f521b2f48cf
S.Z. <234208376+DevSykes@users.noreply.github.com>
2026-03-17 01:26:19 +0100

    feat: ejercicio 4.2

--- ejercicios-dom/ejercicio-04/script.js
+++ ejercicios-dom/ejercicio-04/script.js
@@ -25,4 +25,10 @@ btn.addEventListener("click", (event) => {
 
 //1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
 
+const inputFocus = document.querySelector(".focus");
+
+inputFocus.addEventListener("focus", (event) => {
+  console.log(event.target.value);
+});
+
 //1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.
