3636f325d2a68e0a8b0b2c334e7796248ef1e73f
S.Z. <234208376+DevSykes@users.noreply.github.com>
2026-03-20 00:35:39 +0100

    feat ejercicio 2.3

--- ejercicios-es6-asincronia/ejercicio-02.js
+++ ejercicios-es6-asincronia/ejercicio-02.js
@@ -19,13 +19,16 @@ const toy = { name: "Bus laiyiar", date: "20-30-1995", color: "multicolor" };
 const toyCopy = { ...toy };
 
 console.log(toyCopy);
-// Imprime la copia: { name: "Bus laiyiar", date: "20-30-1995", color: "multicolor" }
 
 /* 2.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando
 spread operatos. */
 
 const pointsList1 = [32, 54, 21, 64, 75, 43];
-const pointsLis2 = [54, 87, 99, 65, 32];
+const pointsList2 = [54, 87, 99, 65, 32];
+
+const combinedPoints = [...pointsList1, ...pointsList2];
+
+console.log(combinedPoints);
 
 /* 2.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos
 con spread operators. */
