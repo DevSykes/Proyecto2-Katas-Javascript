3731fad7971fb9fda6b3a35cedb3cef72b48eab0
S.Z. <234208376+DevSykes@users.noreply.github.com>
2026-03-21 16:26:34 +0100

    feat ejercicio 2

--- ejercicios-es6-asincronia/ejercicio-02/ejercicio-02.js
+++ ejercicios-es6-asincronia/ejercicio-02/ejercicio-02.js
@@ -21,7 +21,7 @@ const toyCopy = { ...toy };
 console.log(toyCopy);
 
 /* 2.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando
-spread operatos. */
+spread operators. */
 
 const pointsList1 = [32, 54, 21, 64, 75, 43];
 const pointsList2 = [54, 87, 99, 65, 32];
