3a0e991c455fa266c5e93fb7616f9db5b18fac97
S.Z. <234208376+DevSykes@users.noreply.github.com>
2026-03-21 01:21:21 +0100

    feat ejercicio 5

--- ejercicios-es6-asincronia/ejercicio-05/ejercicio-05.js
+++ ejercicios-es6-asincronia/ejercicio-05/ejercicio-05.js
@@ -4,7 +4,7 @@ introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si
 introduzco 'i', me deberia de mostrar el streamer 'Rubius' e 'Ibai'. */
 
 const streamers = [
-  { name: "Rubius", age: 32, gameMorePlayed: "Minecraft" },
+  { name: "Rubiuus", age: 32, gameMorePlayed: "Minecraft" },
   { name: "Ibai", age: 25, gameMorePlayed: "League of Legends" },
   { name: "Reven", age: 43, gameMorePlayed: "League of Legends" },
   { name: "AuronPlay", age: 33, gameMorePlayed: "Among Us" },
